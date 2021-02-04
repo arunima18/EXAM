94620bb4db133990502ca36bf357e42378a8962a
arunima18 <42172417+arunima18@users.noreply.github.com>
2021-02-04 13:15:25 +0530

    Update Qn3(1).py

--- Qn3(1).py
+++ Qn3(1).py
@@ -16,9 +16,9 @@ Y=[2.2,1.96,1.72,1.53,1.36,1.22,1.1,1,0.86,0.75,0.65,0.6]
 
 b1,a1,P1=library.linear_fit(X,Y)
 print('The value of w_c for straight line fit came out to be: ')
-print(a1)
-print('The value of w_0 for straight line fit came out to be: ')
 print(b1)
+print('The value of w_0 for straight line fit came out to be: ')
+print(a1)
 print('The pearson factor sor the staright line fit came out to be: ')
 print(P1)
 
@@ -37,9 +37,9 @@ while i<len(Y):
 
 b3,a3,P3=library.linear_fit(X,Z)
 print('The value of w_c for the exponential plot came out to be: ')
-print(a3)
-print('The value of log(w_0) for the exponential fir came out to be: ')
 print(b3)
+print('The value of log(w_0) for the exponential fir came out to be: ')
+print(a3)
 print('The pearson factor for the exponential fit is: ')
 print(P3)
 
@@ -48,14 +48,14 @@ print(P3)
 ################################################################################
 
 '''
-The value of w_c for straight line fit came out to be: 
-2.029102564102564
 The value of w_0 for straight line fit came out to be: 
+2.029102564102564
+The value of w_c for straight line fit came out to be: 
 -0.4747086247086248
 -0.7655606603764613
-The value of w_c for the exponential plot came out to be: 
+The value of w_0 for the exponential plot came out to be: 
 0.7903594771921305
-The value of log(w_0) for the exponential fir came out to be: 
+The value of log(w_c) for the exponential fir came out to be: 
 -0.39563719590281327
 The pearson factor for the exponential fit is: 
 -0.26146415804622253
