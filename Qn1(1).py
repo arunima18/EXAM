2d2422e5ba8c741b4d313b7ab0b8bf771ede6cd6
arunima18 <42172417+arunima18@users.noreply.github.com>
2021-02-04 13:01:50 +0530

    Update Qn1(1).py

--- Qn1(1).py
+++ Qn1(1).py
@@ -29,6 +29,21 @@ for n, v in zip(A, B):
 ans=Wein(B[len(B)-1])
 print('The Weins cosntant b came out to be: ')
 print(ans)
+
+
+########################################################
+'''
+The value of x for the given equation is coming out to be: 
+0 = 0.8383382081137416
+1 = 0.20591157332932686
+2 = 0.014939955086627127
+3 = 8.331991785022301e-05
+4 = 2.6032622327476712e-09
+The Weins cosntant b came out to be: 
+5529184.601556638
+
+
+'''
         
 
         
